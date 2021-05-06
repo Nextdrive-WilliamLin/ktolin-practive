df9388b336ead2ec37200dccf8e181007d474be6
williamlin <william.lin@nextdrive.io>
2021-05-06 12:01:14 +0800

    add dao

---
+++ src/main/kotlin/com/example/demo/dao/Accountdao.kt
@@ -0,0 +1,7 @@
+package com.example.demo.dao
+
+import org.springframework.data.jpa.repository.JpaRepository
+import com.example.demo.entity.AccountType
+
+interface Accountdao: JpaRepository<AccountType, Int> {
+}
--- src/main/kotlin/com/example/demo/entity/Account.kt
+++ src/main/kotlin/com/example/demo/entity/Account.kt
@@ -1,5 +1,10 @@
 package com.example.demo.entity
 
+import javax.persistence.Entity
+import javax.persistence.Table
+
+@Table(name = "account", schema = "bcsms")
+@Entity
 data class AccountType (
     var id: String,
     var name: String,
