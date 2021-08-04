d41721a81718fcc595786b9dcbcce6c3f87aa898
williamlin <william.lin@nextdrive.io>
2021-08-04 16:55:05 +0800

    add upadte func

--- src/main/kotlin/com/example/demo/controller/TestController.kt
+++ src/main/kotlin/com/example/demo/controller/TestController.kt
@@ -1,15 +1,11 @@
 package com.example.demo
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.RequestBody
 import java.util.*
 import com.example.demo.entity.AccountType
 import com.example.demo.dao.AccountRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
 
 @RestController
     @RequestMapping("/api")
@@ -20,14 +16,29 @@ import org.springframework.beans.factory.annotation.Autowired
             return accountRepository.findAll();
         }
 
+        @GetMapping("/account/{id}")
+        fun  getAccountDetail(@PathVariable id: Int): Optional<AccountType> {
+            return accountRepository.findById(id)
+        }
+
+
         @PostMapping("/account")
         fun postAccountData(@RequestBody account: AccountType): AccountType {
             return accountRepository.save(account)
         }
 
-        @PostMapping("/findAccount")
-        fun findAccount(@RequestBody name: String): List<AccountType>? {
-            return accountRepository.findByName(name)
+        @PostMapping("/updateAccount")
+        fun updateAccount(@RequestBody account: AccountType): AccountType {
+            return accountRepository.save(account)
+        }
+
+        @PutMapping("/account/{id}")
+        fun updateAccountByPut(@PathVariable id: Int, @RequestBody data: AccountType): Optional<AccountType> {
+            var account: Optional<AccountType> = accountRepository.findById(id)
+            account.get().email = data.email
+            account.get().name = data.name
+            accountRepository.save(account.get())
+            return account
         }
     }
 
